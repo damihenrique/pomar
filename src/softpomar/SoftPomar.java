8a7c2685cd80f4b956efb67e72e15e3cd40afd31
Dami Henrique <damipelegrino@inatel.br>
2016-12-03 12:16:10 -0200

    testando novamente

--- src/softpomar/SoftPomar.java
+++ src/softpomar/SoftPomar.java
@@ -15,7 +15,8 @@ public class SoftPomar {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("pqp");
+        System.out.println("testandoo");
+        System.out.println("aeae");
     }
     
 }
